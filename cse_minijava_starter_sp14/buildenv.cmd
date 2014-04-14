15ef1a8414b82e79da6c5f73a7d16d545953ab42
hafizusman <usman@uw.edu>
2014-04-14 12:35:11 -0700

    Revert "initial checkin"
    This reverts commit 21cc8618a6f542ee4f3c40e0367e311f4857e606.

--- cse_minijava_starter_sp14/buildenv.cmd
+++
@@ -1,4 +0,0 @@
-set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.7.0_51
-set ANT_HOME=C:\eclipse\ant
-set PATH=%PATH%;%JAVA_HOME%\bin;%ANT_HOME%\bin;c:\eclipse
-set CLASSPATH=lib\CUP.jar;build\classes
