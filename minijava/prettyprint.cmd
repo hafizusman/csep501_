32885ce1c7f3dca8b907fa0323172069aafc3d0f
hafizusman <usman@uw.edu>
2014-04-26 10:29:10 -0700

    prettyprint.cmd stub

---
+++ minijava/prettyprint.cmd
@@ -0,0 +1,1 @@
+?prettyprint.sh/prettyprint.cmd: This should take a MiniJava program on stdin and print a syntactically equivalent version of the program to stdout. This behavior is implemented in PrettyPrintVisitor -- you just need to call it. It should exit with a 0 error 
