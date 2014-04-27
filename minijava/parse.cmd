411ea8b0bb3800179f1628f9f870934c308282b0
hafizusman <usman@uw.edu>
2014-04-27 12:10:58 -0700

    parser.cmd fixed, MyParser.java created

--- minijava/parse.cmd
+++ minijava/parse.cmd
@@ -1,1 +1,5 @@
-This should take a MiniJava program on stdin and print the indented tree version of the AST, described above, to stdout. It should exit with a 0 error code if and only if the parse was successful.
+@ECHO OFF
+
+REM This should take a MiniJava program on stdin and print the indented tree version of the AST, described above, to stdout. It should exit with a 0 error code if and only if the parse was successful.
+
+java -classpath .\build\classes;.\lib\CUP.jar MyParser
