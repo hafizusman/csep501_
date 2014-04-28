0c8f4842391a1c3c84cc76803eae34aea64497dd
hafizusman <usman@uw.edu>
2014-04-27 22:56:56 -0700

    urgh... blocks!!!

--- minijava/MyExample.java
+++ minijava/MyExample.java
@@ -1,4 +1,28 @@
 a = b;
-//b = a + (foo + bar) + baz;
-//System.out.println(baz);
+
+while
+(
+a
+<
+12
+)
+{
+u
+=
+10
+;
+}
+
+//if (x < 10) { m = 12; } else { m = 9; }
+b = a + (foo + bar) + baz;
+System.out.println(baz);
 System.out.println(b);
+
+a = 12;
+x = true;
+x = this;
+a[10] = 999;
+y = a.length;
+p = new int[100];
+q = new MyClass();
+//x = q.myFunc();
