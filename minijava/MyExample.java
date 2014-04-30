7d400b421b7a277dda674e4bbbfcc1154d217624
hafizusman <usman@uw.edu>
2014-04-29 18:10:46 -0700

    First draft ready for submit, need to clean prpr

--- minijava/MyExample.java
+++ minijava/MyExample.java
@@ -1,79 +1,17 @@
-class MyMainClass
-{
-    public static void main(String [] args)
-    {
-        System.out.println(new MyOtherClass());
+class Factorial{
+    public static void main(String[] a){
+        System.out.println(new Fac().ComputeFac(10));
     }
 }
 
+class Fac {
 
-class FooClass extends AClass
-{
-    int a;
-
-    public boolean bfunc()
-    {
-        x = 10;
-        return xyz;
-    }
-}
-
-class AClass
-{
-    int class_var;
-
-    public int AnotherMethodToParse() {
-        int x;
-
-        y = z + 10;
-        return 100 - i10;
-    }
-
-    public int TheMethodToParse(int[] myarr, int myint, boolean mybool) {
-        int a;
-        boolean b;
-        int[] c;
-
-        while(a<12)
-        {
-            a = b;
-            c2 = c && d;
-            u = my.yourfunc();
-        }
-
-        while(!b)
-        {}
-
-
-        if (x < 10) {
-            m = 12;
-        } else {
-            m = 9;
-        }
-
-
-/*
-x = q.myFunc(a+100, cd, a[10], new obj(), another.function(call));
-c3 = !f && g;
-h = h1 - h2;
-i = 12;
-j = j3 * j4;
-j = i < 10;
-x = true && !f;
-x = this;
-a[x - y] = 10 - b[i-4];
-y = a.length;
-p = new int[100];
-p2 = new int[x - 100];
-q = new MyClass();
-b = a + (foo + bar) + baz;
-System.out.println(baz);
-System.out.println(b);
-
-//    a = b;
-//    y = 12;
-//    x = q.myFunc(a+100 - 20*cd, cd, a[10], new obj(), another.function(call));
-*/
-        return f;
+    public int ComputeFac(int num) {
+        int num_aux;
+        if (num < 1)
+            num_aux = 1;
+        else
+            num_aux = num * (this.ComputeFac(num - 1));
+        return num_aux;
     }
 }
--- minijava/src/MyParser.java
+++ minijava/src/MyParser.java
@@ -16,39 +16,19 @@ public class MyParser {
     public static void main(String [] args) {
         try {
             // create a scanner on the input file
-            //scanner s = new scanner(System.in);
-            scanner s = new scanner(new FileReader("C:\\eclipse\\csep501_\\minijava\\MyExample.java"));
+            scanner s = new scanner(System.in);
+            //scanner s = new scanner(new FileReader("C:\\eclipse\\csep501_\\minijava\\MyExample.java"));
             parser p = new parser(s);
             Symbol root;
-	    // replace p.parse() with p.debug_parse() in next line to see trace of
-	    // parser shift/reduce actions during parse
-            if (false)
-            {
-                root = p.parse();
-                //root = p.debug_parse();
-                ArrayList<Statement> program = (ArrayList<Statement>)root.value;
-                for (Statement statement: program) {
-                    statement.accept(new PrettyPrintVisitor());
-                    System.out.print("\n");
-                }
-            }
-            else
-            {
-                root = p.parse();
-                //root = p.debug_parse();
-                if (true) {
-                    Program program = (Program) root.value;
-                    program.accept(new PrettyPrintVisitor());
-                }
-                else {
-                    ClassDeclList program = (ClassDeclList) root.value;
-                    for (int i = 0; i < program.size(); i++) {
-                        ClassDecl d = program.get(i);
-                        d.accept(new PrettyPrintVisitor());
-                        System.out.print("\n");
-                    }
-                }
-            }
+
+            // replace p.parse() with p.debug_parse() in next line to see trace of
+            // parser shift/reduce actions during parse
+            root = p.parse();
+            //root = p.debug_parse();
+
+            Program program = (Program) root.value;
+            program.accept(new PrettyPrintVisitor());
+
             System.out.print("\nParsing completed");
         } catch (Exception e) {
             // yuck: some kind of error in the compiler implementation
